7fc9606218f5ac4c65a1494619c7abd6b4941ec2
Weipeng <13546782483@163.com>
2018-05-11 13:59:00 +0800

    先判断session session没有数据再判断cookie

--- empShow/src/main/java/filter/EmpFilter.java
+++ empShow/src/main/java/filter/EmpFilter.java
@@ -1,12 +1,19 @@
 package filter;
 
+import dao.LoginMapper;
 import entity.User;
+import org.apache.ibatis.session.SqlSession;
+import util.CookieUtil;
+import util.Sqlsession;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
 
 @WebFilter(urlPatterns = {"/*"})
 public class EmpFilter implements Filter {
@@ -23,9 +30,30 @@ public class EmpFilter implements Filter {
                 ||uri.endsWith(".gif")||uri.endsWith("loginpp")){
             filterChain.doFilter(servletRequest,servletResponse);
         }else{
-            User user = (User) request.getSession().getAttribute("user");
+            HttpSession httpSession = request.getSession();
+            User user = (User) httpSession.getAttribute("user");
+//            session中没有数据
             if(user==null){
-                ((HttpServletResponse)servletResponse).sendRedirect("login.jsp");
+//                判断Cookie
+                Cookie[] cookies= request.getCookies();
+//                从这个数组中找 不好找，放到map集合中，
+                Map<String,Cookie> cookieMap = CookieUtil.getCookieMap(cookies);
+                Cookie userCookie = cookieMap.get("username");
+
+               if(userCookie!=null){
+                   String username=userCookie.getValue();
+
+                   SqlSession sqlSession = Sqlsession.getSqlSession(true);
+                   LoginMapper loginMapper =sqlSession.getMapper(LoginMapper.class);
+                   User u= loginMapper.login(username);
+//                      找到后再存到session中 emplist中要用
+                   httpSession.setAttribute("user",u);
+
+                   filterChain.doFilter(servletRequest,servletResponse);
+               }else{
+                   ((HttpServletResponse)servletResponse).sendRedirect("login.jsp");
+               }
+
             }else{
                 filterChain.doFilter(servletRequest,servletResponse);
             }
