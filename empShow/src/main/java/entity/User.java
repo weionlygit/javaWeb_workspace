b12b54c5fd780ba5eb588d4b90ae6023c33a7afe
Weipeng <13546782483@163.com>
2018-05-16 20:39:21 +0800

    添加上传图片地址

--- empShow/src/main/java/entity/User.java
+++ empShow/src/main/java/entity/User.java
@@ -1,11 +1,16 @@
 package entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String name;
     private String password;
     private String realname;
     private String sex;
+    private String img_path;
 }
